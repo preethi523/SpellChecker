c1db051f49d8d1e360490f9dbac4e50fcd1d33b9
preethi <preethianbhu23@gmail.com>
2022-02-21 15:29:28 +0530

    spellCorrection

--- src/main/scala/SpellChecker.scala
+++ src/main/scala/SpellChecker.scala
@@ -1,11 +1,14 @@
 import Trie.Trie
 import com.github.vickumar1981.stringdistance.StringConverter._
-import java.nio.charset.{CodingErrorAction, StandardCharsets}
-import scala.io.{BufferedSource, Codec, Source}
+
+import java.nio.charset.CodingErrorAction
+import scala.io.{Codec, Source}
 object Trie {
   class Trie[Val] {
     private var store = new Node(Map(), None)
+
     def put(key: String, value: Val): Unit = store = store.put(key, value, 0)
+
     def get(key: String): Option[Val] = store.get(key).contents
 
     private class Node(kids: Map[Char, Node], val contents: Option[Val]) {
@@ -34,43 +37,48 @@ object Trie {
         f(this, vals, prefix)
       }
     }
-    def editDistance( s:String,dic:String)={
+    def editDistance(s: String, dic: String): Int = {
       val damerauDist: Int = s.damerauDist(dic)
       damerauDist
     }
+    def spellCorrection(s: String, path: String) = {
+      implicit val codec = Codec("UTF-8")
+      codec.onMalformedInput(CodingErrorAction.REPLACE)
+      codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+      val lines = Source.fromFile(path).getLines().toList
+      for (line <- lines.indices) {
+
+       if( editDistance(s, lines(line))==1) print(lines(line)+" ")
+
+       //else print(editDistance(s,lines(line)))
+      }
+
+    }
   }
-
 }
-
-
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    val p = new Trie[Int]
-    val path = "C:\\Users\\hai\\Desktop\\engmix.txt"
-
-    implicit val codec = Codec("UTF-8")
-    codec.onMalformedInput(CodingErrorAction.REPLACE)
-    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
-    val a= System.currentTimeMillis()
-    def insertdic()= {
+  object Main {
+    def main(args: Array[String]): Unit = {
+      val p = new Trie[Int]
+      val path = "C:\\Users\\hai\\Desktop\\engmix.txt"
+      implicit val codec = Codec("UTF-8")
+      codec.onMalformedInput(CodingErrorAction.REPLACE)
+      codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+      val a = System.currentTimeMillis()
       val lines = Source.fromFile(path).getLines().toList
       for (line <- lines.indices) {
         // println(lines(line))
         (p.put(lines(line), 1))
       }
+      val inputString = "This is an Apple. It is good for health. An Appple a day keeps doctor awau"
+      val input1 = inputString.toLowerCase.trim()
+       val input =input1.replace(".","")
+      val s = input.split(" ")
+      for (i <- 0 until s.length) {
+        if (p.get(s(i)) != None) print(s(i) + " ")
+        else p.spellCorrection(s(i), path)
+      }
+      val b = System.currentTimeMillis()
+      val diff = b - a
+      print(diff)
     }
-     val inputString="This is an Anpple"
-
-    val input= inputString.toLowerCase.trim()
-    val s= input.split(" ")
-    for(i<- 0 until s.length){
-      if( p.get(s(i)) != None ){print(s(i)+" ")}
-     // else (p.editDistance(s(i),p.get("hii"))
-    }
-
-    val b= System.currentTimeMillis()
-    val diff=a-b
-    print(diff)
-  }
 }
