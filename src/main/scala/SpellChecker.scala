dff7cde0265c7f8bac158018d193133a669a9110
preethia <preethianbhu23@gmail.com>
2022-03-01 11:53:40 +0530

    testcases

--- src/main/scala/SpellChecker.scala
+++ src/main/scala/SpellChecker.scala
@@ -37,11 +37,6 @@ object Trie {
 
       def get(key: String): Node = get(key, 0)
 
-      def traverse[T](f: (Node, Stream[T], String) => T, prefix: String): T = {
-        val keys = kids.keys.toVector.sorted.toStream
-        val vals = keys.map(key => children(key).traverse[T](f, prefix ++ key.toString))
-        f(this, vals, prefix)
-      }
     }
     /** *
 //     * it corrects the incorrect word using dictionary list
@@ -80,60 +75,86 @@ object Trie {
 
 object Main {
   val p = new Trie[Int]
+
   def main(args: Array[String]): Unit = {
     val path = "/home/preethia/Desktop/SpellChecker/engmix.txt"
-    val a = System.currentTimeMillis()
-    val DictionaryList = readDictionary(path)
-    while(true){
-      println("Enter your input:")
-      val inputString = scala.io.StdIn.readLine()
-      println("Is given input is :" + inputString)
-      val inputLowercase = inputString.toLowerCase.trim()
-      val input = inputLowercase.replace("." , "")
-      val s = input.split(" ")
-      val t = s.zipWithIndex
-      val arr = new Array[String](s.length)
+    val dictionaryList = readDictionary(path)
+    var flag=true
+    while (flag) {
+      println("Enter 1 for spellchecking and anything for exit" )
+      val option = scala.io.StdIn.readInt()
+      option match{
+        case 1 => println("Enter your input:")
+                val inputString = scala.io.StdIn.readLine()
+                 println("Is given input is :" + inputString)
+                println(  checkString(inputString,dictionaryList))
+        case _ => print("Exited")
+                  flag=false
 
-      var correctedInput= " "
-      for (i <- 0 until s.length) {
-        if (p.get(t(i)._1) != None) {
-          arr(t(i)._2) = t(i)._1
-        }
-        else {
-          val q  = p.correctingWord(t(i)._1, DictionaryList)
-          arr(t(i)._2) = q
-        }
       }
-      println(arr.mkString(" "))
     }
-    val b = System.currentTimeMillis()
-    val diff = b - a
-    print(diff)
   }
 
 
-
-  /***reads the contents in the dictionary file and insert it into trie data structure
+  /** *
+   * check the given string is present in dictionary or not.
    *
-   * @param path:dictionary path
+   * @param inputString        :The phrase given by user
+   * @param dictionaryList  :dictionary words stored in list
+   * @return the full corrected phrase is returned
+   */
+def checkString(inputString:String,dictionaryList:List[String]):String={
+  val loggerObj = new Logger()
+  loggerObj.logger.info("function check string started")
+  loggerObj.startTime()
+    val inputLowercase = inputString.toLowerCase.trim()
+    val input = inputLowercase.replace(".", "")
+    val s = input.split(" ")
+    val t = s.zipWithIndex
+    val arr = new Array[String](s.length)
+    for (i <- 0 until s.length) {
+      if (p.get(t(i)._1) != None) {
+        arr(t(i)._2) = t(i)._1
+      }
+      else {
+        val q = p.correctingWord(t(i)._1, dictionaryList)
+        arr(t(i)._2) = q
+      }
+    }
+  loggerObj.stopTime()
+  loggerObj.logger.info("function check string ended" + loggerObj.getTime)
+    arr.mkString(" ")
+  }
+
+
+  /** *reads the contents in the dictionary file and insert it into trie data structure
+   *
+   * @param path  :dictionary path
    * @return dictionarywords in list
    */
-  def readDictionary(path:String)={
+  def readDictionary(path: String): List[String] = {
     val loggerObj = new Logger()
-    loggerObj.logger.info("function readdictionary started")
+    loggerObj.logger.info("function read dictionary started")
     loggerObj.startTime()
     implicit val codec = Codec("UTF-8")
     codec.onMalformedInput(CodingErrorAction.REPLACE)
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
-    // val a = System.currentTimeMillis()
-    var lines= Source.fromFile(path).getLines().toList
-    for (line <- lines.indices) {
-      p.put(lines(line), 1)
+    var dictionaryList = List(" ")
+    val source = Source.fromFile(path)
+    if (source.isEmpty== true) {
+      return List("file is empty")
     }
-
-    loggerObj.stopTime()
-    loggerObj.logger.info("function read dictionary ended" + loggerObj.getTime)
-    lines
+    else {
+      var lines = source.getLines()
+      var dictionaryList = lines.toList
+      for (line <- dictionaryList.indices) {
+        p.put(dictionaryList(line), 1)
+      }
+      source.close
+      loggerObj.stopTime()
+      loggerObj.logger.info("function read dictionary ended" + loggerObj.getTime)
+      return dictionaryList
+    }
   }
 }
 
